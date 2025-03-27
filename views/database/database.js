e3ef81ff77f5eecb8f75ef55a9700ddcb267bf51
Samuel Campelo <168687328+SamCampel@users.noreply.github.com>
2025-03-27 16:32:13 -0300

    Update

--- views/database/database.js
+++ views/database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 //através do sequelize é posiivel criar tabelas no banco de dado usando javascript
-const connection = new Sequelize('guiaperguntas','root','senha1234',{
+const connection = new Sequelize('guiaperguntas','root','',{
     host: 'localhost',
     dialect: 'mysql'
 });
